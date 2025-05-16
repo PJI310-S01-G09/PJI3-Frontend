413a7019ca86cde649bab8729d8448378cd8f99e
José Lourenço <3s3phinxil@gmail.com>
2025-05-16 00:58:50 -0300

    feat: filtering sidebar items when logged in

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -1,27 +1,42 @@
-import { AppBar, Button, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material";
+"use client";
+
+import { useAuth } from "@/contexts/AuthContext";
+import {
+  AppBar,
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+const sideBarItems = [
+  { text: "Agendar", href: "/", mustBeLoggedIn: false },
+  { text: "Agendamentos", href: "/agendamentos", mustBeLoggedIn: true },
+  { text: "Inventário", href: "/inventario", mustBeLoggedIn: true },
+];
 
 export default function Sidebar() {
+  const { isAuthenticated } = useAuth();
+  const mappedSidebar = sideBarItems.filter((sidebar) =>
+    sidebar.mustBeLoggedIn ? isAuthenticated : true
+  );
   return (
     <Drawer variant="permanent" anchor="left" className="z-10">
       <div className="w-64 h-full bg-white shadow-md pt-16">
         <List>
-          <ListItem disablePadding>
-            <ListItemButton href="/">
-              <ListItemText primary="Agendar" />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem disablePadding>
-            <ListItemButton href="/agendamentos">
-              <ListItemText primary="Agendamentos" />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem disablePadding>
-            <ListItemButton href="/inventario">
-              <ListItemText primary="Inventário" />
-            </ListItemButton>
-          </ListItem>
+          {mappedSidebar.map((item, index) => (
+            <ListItem disablePadding key={`sidebarItem${index}`}>
+              <ListItemButton href={item.href}>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
         </List>
       </div>
     </Drawer>
