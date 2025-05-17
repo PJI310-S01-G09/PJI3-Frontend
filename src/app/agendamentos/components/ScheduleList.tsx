6696b627b71cd3cac7f7eff888a40157b84ed03e
José Lourenço <3s3phinxil@gmail.com>
2025-05-17 10:32:45 -0300

    feat: styles on schedules list

--- src/app/agendamentos/components/ScheduleList.tsx
+++ src/app/agendamentos/components/ScheduleList.tsx
@@ -4,16 +4,35 @@ import { useAuth } from "@/contexts/AuthContext";
 import { useEffect, useState } from "react";
 import { getSchedules } from "@/services/api/schedule";
 import { Schedule } from "@/entities/schedule.entity";
+import {
+  Box,
+  Typography,
+} from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { Box, Typography } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 export const ScheduleList = () => {
   const { token } = useAuth();
   const [scheduleData, setScheduleData] = useState<Schedule[]>([]);
+  const [filteredScheduleData, setFilteredScheduleData] = useState<Schedule[]>([]);
+  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
 
   useEffect(() => {
     fetchSchedule();
   }, [token]);
+
+  useEffect(() => {
+    if (selectedDate) {
+      setFilteredScheduleData(
+        scheduleData.filter((s) =>
+          dayjs(s.scheduledAt).isSame(selectedDate, "day")
+        )
+      );
+    }
+  }, [selectedDate])
 
   const fetchSchedule = async () => {
     if (!token) return;
@@ -27,6 +46,10 @@ export const ScheduleList = () => {
     setScheduleData(response.data);
   };
 
+  const agendamentoDates = new Set(
+    scheduleData.map((s) => s.scheduledAt.slice(0, 10))
+  );
+
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID", width: 70 },
     {
@@ -34,6 +57,12 @@ export const ScheduleList = () => {
       headerName: "Cliente",
       flex: 1,
       valueGetter: (params, row) => row.client.name,
+    },
+    {
+      field: "cpf",
+      headerName: "CPF",
+      flex: 1,
+      valueGetter: (params, row) => row.client.cpf,
     },
     {
       field: "phone",
@@ -66,24 +95,38 @@ export const ScheduleList = () => {
       <Typography variant="h4" gutterBottom>
         Agendamentos
       </Typography>
-      {scheduleData.length > 0 && (
-        <Box sx={{ height: 600, width: "100%" }}>
-          <DataGrid
-            rows={scheduleData}
-            columns={columns}
-            initialState={{
-            pagination: {
-                paginationModel: {
-                pageSize: 10,
-                },
-            },
-            }}
-            pageSizeOptions={[10]}
-            getRowId={(row) => row.id}
-            disableRowSelectionOnClick
+
+      <Box display="flex" flexDirection={{ xs: "column", md: "row" }} gap={4}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateCalendar
+            value={selectedDate}
+            onChange={(newDate) => setSelectedDate(newDate)}
+            shouldDisableDate={(day) =>
+              !agendamentoDates.has(day.format("YYYY-MM-DD"))
+            }
           />
+        </LocalizationProvider>
+
+        <Box flex={1}>
+          {filteredScheduleData.length > 0 && (
+            <Box sx={{ height: 600, width: "100%" }}>
+              <DataGrid
+                rows={filteredScheduleData}
+                columns={columns}
+                initialState={{
+                  pagination: { paginationModel: { pageSize: 10 } },
+                }}
+                pageSizeOptions={[10]}
+                getRowId={(row) => row.id}
+                disableRowSelectionOnClick
+              />
+            </Box>
+          )}
+          {
+            !selectedDate && "Selecione uma data para exibir os agendamentos do dia"
+          }
         </Box>
-      )}
+      </Box>
     </Box>
   );
 };
